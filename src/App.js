ba60ce9eee51675af65e26e728e7799e91559d46
ThemeGravity <themegravity@gmail.com>
2020-09-01 23:07:19 +0200

    Add header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
