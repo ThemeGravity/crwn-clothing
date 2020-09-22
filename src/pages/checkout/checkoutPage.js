4e351c4c2887cb20a32e67a5b2440a83065ceef2
ThemeGravity <themegravity@gmail.com>
2020-09-22 22:27:41 +0200

    Add Stripe integration

--- src/pages/checkout/checkoutPage.js
+++ src/pages/checkout/checkoutPage.js
@@ -4,6 +4,7 @@ import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selector";
 import './checkout.styles.scss';
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
+import StripeButton from "../../components/stripe-button/StripeButton";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -28,6 +29,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br/>
+      4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+    </div>
+    <StripeButton price={total} />
   </div>
 );
 
